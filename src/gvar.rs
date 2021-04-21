1a916b5c081ef9d7eeeb99e272a588d1309d24fe
Simon Cozens <simon@simon-cozens.org>
2021-04-21 09:57:58 +0100

    Tidy with macros (still implementation incomplete)

--- src/gvar.rs
+++ src/gvar.rs
@@ -1,10 +1,13 @@
 use otspec::de::CountedDeserializer;
 use otspec::de::Deserializer as OTDeserializer;
+use otspec::{deserialize_visitor, read_field, read_field_counted, read_remainder};
+use serde::de::DeserializeSeed;
 use serde::de::SeqAccess;
 use serde::de::Visitor;
 use serde::ser::SerializeSeq;
 use serde::{Deserialize, Serialize};
 use serde::{Deserializer, Serializer};
+use std::convert::TryInto;
 extern crate otspec;
 use otspec::types::*;
 use otspec_macros::tables;
@@ -33,45 +36,21 @@ pub struct gvar {
     glyphVariations: Vec<GlyphVariationData>,
 }
 
-struct GvarVisitor {
-    _phantom: std::marker::PhantomData<gvar>,
-}
-
-impl GvarVisitor {
-    fn new() -> Self {
-        GvarVisitor {
-            _phantom: std::marker::PhantomData,
-        }
-    }
-}
-impl<'de> Visitor<'de> for GvarVisitor {
-    type Value = gvar;
-
-    fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(formatter, "A sequence of values")
-    }
-
+deserialize_visitor!(
+    gvar,
+    GvarVisitor,
     fn visit_seq<A: SeqAccess<'de>>(self, mut seq: A) -> Result<Self::Value, A::Error> {
-        let core = seq
-            .next_element::<GvarHeader>()?
-            .ok_or_else(|| serde::de::Error::custom("Expecting a gvar table"))?;
+        let core = read_field!(seq, GvarHeader, "a gvar table header");
         let mut dataOffsets: Vec<u32> = Vec::new();
-        for _i in 0..(core.glyphCount + 1) {
-            if core.flags & 0x1 == 0 {
-                let offset = seq.next_element::<u16>()?.ok_or_else(|| {
-                    serde::de::Error::custom("Expecting a glyphVariationDataOffset")
-                })?;
-                dataOffsets.push((offset * 2).into());
-            } else {
-                let offset = seq.next_element::<u32>()?.ok_or_else(|| {
-                    serde::de::Error::custom("Expecting a glyphVariationDataOffset")
-                })?;
-                dataOffsets.push(offset);
-            }
+        if core.flags & 0x1 == 0 {
+            // u16 offsets, need doubling
+            let u16_and_halved: Vec<u16> =
+                read_field_counted!(seq, core.glyphCount, "a glyphVariationDataOffset");
+            dataOffsets = u16_and_halved.iter().map(|x| (x * 2).into()).collect();
+        } else {
+            dataOffsets = read_field_counted!(seq, core.glyphCount, "a glyphVariationDataOffset");
         }
-        let remainder = seq
-            .next_element::<Vec<u8>>()?
-            .ok_or_else(|| serde::de::Error::custom("Expecting a gvar table"))?;
+        let remainder = read_remainder!(seq, "a gvar table");
         let offset_base: usize = 20;
         let axis_count = core.axisCount as usize;
 
@@ -80,27 +59,30 @@ impl<'de> Visitor<'de> for GvarVisitor {
         let mut shared_tuple_start = (core.sharedTuplesOffset as usize) - offset_base;
         let shared_tuple_end =
             shared_tuple_start + (core.sharedTupleCount * core.axisCount * 2) as usize;
-        // while shared_tuple_start < shared_tuple_end {
-        //     let bytes = &remainder[shared_tuple_start..shared_tuple_start + 2 * axis_count];
-        //     let mut de = OTDeserializer::from_bytes(bytes);
-        //     println!("Trying to deserialize shared tuple array {:?}", bytes);
-        //     let cs: CountedDeserializer<i16> = CountedDeserializer::with_len(axis_count);
-        //     let tuple: Vec<f32> = cs
-        //         .deserialize(de)
-        //         .map_err(|_| serde::de::Error::custom("Expecting a tuple"))?
-        //         .iter()
-        //         .map(|i| *i as f32 / 16384.0)
-        //         .collect();
-        //     println!("Tuple {:?}", tuple);
-        //     shared_tuple_start += 2 * axis_count;
-        //     shared_tuples.push(tuple);
-        // }
+        while shared_tuple_start < shared_tuple_end {
+            let bytes = &remainder[shared_tuple_start..shared_tuple_start + 2 * axis_count];
+            let mut de = OTDeserializer::from_bytes(bytes);
+            println!("Trying to deserialize shared tuple array {:?}", bytes);
+            let cs: CountedDeserializer<i16> = CountedDeserializer::with_len(axis_count);
+            let tuple: Vec<f32> = cs
+                .deserialize(&mut de)
+                .map_err(|_| serde::de::Error::custom("Expecting a tuple"))?
+                .iter()
+                .map(|i| *i as f32 / 16384.0)
+                .collect();
+            println!("Tuple {:?}", tuple);
+            shared_tuple_start += 2 * axis_count;
+            shared_tuples.push(tuple);
+        }
 
         /* Glyph offsets */
-        for i in 0..(core.glyphCount + 1) {
+        for i in 0..(core.glyphCount) {
             println!("Glyph {:?} offset {:?}", i, dataOffsets[i as usize]);
-            let offset = dataOffsets[i as usize] + (core.glyphVariationDataArrayOffset) - 20;
-            // let bytes =
+            let offset: usize = (dataOffsets[i as usize] + (core.glyphVariationDataArrayOffset)
+                - 20)
+                .try_into()
+                .unwrap();
+            let bytes = &remainder[offset..];
         }
 
         Ok(gvar {
@@ -111,16 +93,7 @@ impl<'de> Visitor<'de> for GvarVisitor {
             glyphVariations: vec![],
         })
     }
-}
-
-impl<'de> Deserialize<'de> for gvar {
-    fn deserialize<D>(d: D) -> Result<Self, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        d.deserialize_seq(GvarVisitor::new())
-    }
-}
+);
 
 #[cfg(test)]
 mod tests {
