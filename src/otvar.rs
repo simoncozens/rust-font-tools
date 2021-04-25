8469aa81f1d208cc076e7e38f9d8f2e44c9fb770
Simon Cozens <simon@simon-cozens.org>
2021-04-25 21:08:59 +0100

    Deserialise TupleVariationHeader

--- src/otvar.rs
+++ src/otvar.rs
@@ -1,13 +1,13 @@
+use crate::glyf::Glyph;
 use otspec::de::CountedDeserializer;
-use otspec::ser;
+use serde::de::DeserializeSeed;
 use serde::de::SeqAccess;
 use serde::de::Visitor;
-use serde::ser::SerializeSeq;
 use serde::Deserializer;
-use serde::Serializer;
 use serde::{Deserialize, Serialize};
-use std::convert::TryInto;
+use std::fmt;
 extern crate otspec;
+use bitflags::bitflags;
 use otspec::types::*;
 use otspec::{deserialize_visitor, read_field, read_field_counted, read_remainder};
 use otspec_macros::tables;
@@ -122,6 +122,94 @@ deserialize_visitor!(
         })
     }
 );
+
+bitflags! {
+    #[derive(Serialize, Deserialize)]
+    pub struct TupleIndexFlags: u16 {
+        const EMBEDDED_PEAK_TUPLE = 0x8000;
+        const INTERMEDIATE_REGION = 0x4000;
+        const PRIVATE_POINT_NUMBERS = 0x2000;
+        const TUPLE_INDEX_MASK = 0x0FFF;
+    }
+}
+
+pub struct TupleVariationHeader {
+    size: uint16,
+    flags: TupleIndexFlags,
+    sharedTupleIndex: uint16,
+    peakTuple: Option<Vec<f32>>,
+    startTuple: Option<Vec<f32>>,
+    endTuple: Option<Vec<f32>>,
+}
+
+pub struct TupleVariationHeaderDeserializer {
+    axisCount: uint16,
+}
+
+impl<'de> DeserializeSeed<'de> for TupleVariationHeaderDeserializer {
+    type Value = TupleVariationHeader;
+
+    fn deserialize<D>(self, deserializer: D) -> std::result::Result<Self::Value, D::Error>
+    where
+        D: serde::de::Deserializer<'de>,
+    {
+        struct TupleVariationHeaderVisitor {
+            axisCount: uint16,
+        }
+
+        impl<'de> Visitor<'de> for TupleVariationHeaderVisitor {
+            type Value = TupleVariationHeader;
+
+            fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+                write!(formatter, "a tuple variation store")
+            }
+
+            fn visit_seq<A>(self, mut seq: A) -> std::result::Result<TupleVariationHeader, A::Error>
+            where
+                A: SeqAccess<'de>,
+            {
+                let mut res = TupleVariationHeader {
+                    peakTuple: None,
+                    startTuple: None,
+                    endTuple: None,
+                    flags: TupleIndexFlags::empty(),
+                    size: 0,
+                    sharedTupleIndex: 0,
+                };
+                res.size = read_field!(seq, uint16, "a table size");
+                res.flags = read_field!(seq, TupleIndexFlags, "a tuple index");
+                res.sharedTupleIndex = res.flags.bits() & TupleIndexFlags::TUPLE_INDEX_MASK.bits();
+                if res.flags.contains(TupleIndexFlags::EMBEDDED_PEAK_TUPLE) {
+                    res.peakTuple = Some(
+                        (read_field_counted!(seq, self.axisCount, "a peak tuple") as Vec<i16>)
+                            .iter()
+                            .map(|x| F2DOT14::unpack(*x))
+                            .collect(),
+                    );
+                }
+                if res.flags.contains(TupleIndexFlags::INTERMEDIATE_REGION) {
+                    res.startTuple = Some(
+                        (read_field_counted!(seq, self.axisCount, "a start tuple") as Vec<i16>)
+                            .iter()
+                            .map(|x| F2DOT14::unpack(*x))
+                            .collect(),
+                    );
+                    res.endTuple = Some(
+                        (read_field_counted!(seq, self.axisCount, "an end tuple") as Vec<i16>)
+                            .iter()
+                            .map(|x| F2DOT14::unpack(*x))
+                            .collect(),
+                    );
+                }
+                Ok(res)
+            }
+        }
+
+        deserializer.deserialize_seq(TupleVariationHeaderVisitor {
+            axisCount: self.axisCount,
+        })
+    }
+}
 
 #[cfg(test)]
 mod tests {
