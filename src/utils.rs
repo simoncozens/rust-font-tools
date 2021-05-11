b63d774bf8d4af37590f3472ef1ca33e09c99c12
Marc Foley <m.foley.88@gmail.com>
2021-05-11 13:05:55 +0100

    fix int_list_to_num

--- src/utils.rs
+++ src/utils.rs
@@ -1,7 +1,7 @@
 pub fn int_list_to_num(int_list: &[u8]) -> u32 {
     let mut flags = 0;
     for flag in int_list {
-        flags += 1 << (flag + 1);
+        flags |= (1 << flag);
     }
     flags
 }
