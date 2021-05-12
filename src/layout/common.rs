2c48cfca08b1178f7d045cb48da217b051e7c276
Simon Cozens <simon@simon-cozens.org>
2021-05-12 10:58:38 +0100

    Lookup flags

--- src/layout/common.rs
+++ src/layout/common.rs
@@ -1,3 +1,4 @@
+use bitflags::bitflags;
 use otspec::types::*;
 use otspec_macros::tables;
 use serde::{Deserialize, Serialize};
@@ -39,7 +40,7 @@ tables!(
     }
     Lookup {
             uint16	lookupType
-            uint16	lookupFlag
+            LookupFlags	lookupFlag
             Counted(uint16)	subtableOffsets
             // Optional markFilteringSet
     }
@@ -69,4 +70,23 @@ pub enum FeatureParams {
     StylisticSet(uint16, uint16),
     SizeFeature(sizeFeatureParams),
     CharacterVariant(cvFeatureParams),
+}
+
+bitflags! {
+    /// Lookup qualifiers
+    #[derive(Serialize, Deserialize)]
+    pub struct LookupFlags: u16 {
+        /// Position the last glyph of a cursive positioning sequence on the baseline
+        const RIGHT_TO_LEFT = 0x0001;
+        /// Skip over base glyphs
+        const IGNORE_BASE_GLYPHS = 0x0002;
+        /// Skip over ligatures
+        const IGNORE_LIGATURES = 0x0004;
+        /// Skip over all combining marks
+        const IGNORE_MARKS = 0x0008;
+        /// Indicates that the lookup table structure is followed by a MarkFilteringSet field
+        const USE_MARK_FILTERING_SET = 0x0010;
+        /// Mask off the high bits to reveal a mark class defined in the GDEF table
+        const MARK_ATTACHMENT_TYPE_MASK = 0xFF00;
+    }
 }
