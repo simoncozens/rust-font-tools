ded94e23be57e507f290546cb721440760bf44e4
Simon Cozens <simon@simon-cozens.org>
2021-05-13 17:28:31 +0100

    Attempt to fix avar mapping

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ mod utils;
 
 use buildbasic::{build_font, build_fonts};
 use clap::{App, Arg};
+use designspace::Designspace;
 use rayon::prelude::*;
 use std::fs::File;
 use std::io;
@@ -32,19 +33,22 @@ fn main() {
 
     if filename.ends_with(".designspace") {
         let ds = designspace::from_file(filename).expect("Couldn't parse designspace");
+        let default_location = ds.default_designspace_location();
         let dm_index = ds
             .sources
             .source
             .iter()
-            .position(|s| ds.source_location(s) == ds.default_location())
-            .expect("Couldn't find default master");
+            .position(|s| ds.source_location(s) == default_location);
+        if dm_index.is_none() {
+            default_master_not_found_error(ds);
+        }
         let masters: Vec<norad::Font> = ds
             .sources
             .source
             .par_iter()
             .map(|s| s.ufo().expect("Couldn't open master file"))
             .collect();
-        font = build_fonts(dm_index, masters, ds.variation_model());
+        font = build_fonts(dm_index.unwrap(), masters, ds.variation_model());
         ds.add_to_font(&mut font)
             .expect("Couldn't add variation tables");
     } else if filename.ends_with(".ufo") {
@@ -61,4 +65,29 @@ fn main() {
     } else {
         font.save(&mut io::stdout());
     };
+}
+
+fn default_master_not_found_error(ds: Designspace) -> ! {
+    let default_location = ds.default_location();
+    let location_to_string = |location: Vec<i32>| {
+        ds.axis_order()
+            .iter()
+            .zip(location.iter())
+            .map(|(tag, value)| format!("{:}={:}", std::str::from_utf8(tag).unwrap(), value))
+            .collect::<Vec<String>>()
+            .join(", ")
+    };
+    log::error!(
+        "Could not find default master [{:}]",
+        location_to_string(default_location)
+    );
+    eprintln!("Master locations were: ");
+    for source in &ds.sources.source {
+        eprintln!(
+            "{:} = {:}",
+            source.filename,
+            location_to_string(ds.source_location(source))
+        );
+    }
+    std::process::exit(1);
 }
