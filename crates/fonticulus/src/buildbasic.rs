8f0b6b970bc77d90c1b40a7681874190dd26fed0
Simon Cozens <simon@simon-cozens.org>
2021-05-16 22:24:04 +0100

    Decompose mixed glyphs at UFO level.
    Code stolen from https://github.com/linebender/norad/pull/80

--- crates/fonticulus/src/buildbasic.rs
+++ crates/fonticulus/src/buildbasic.rs
@@ -6,20 +6,67 @@ use fonttools::glyf;
 use fonttools::gvar::GlyphVariationData;
 use fonttools::hmtx;
 use fonttools::otvar::VariationModel;
+use kurbo::{Affine, Point};
+use norad::{Component, Contour, ContourPoint, Glyph, Layer};
 use std::collections::{BTreeMap, HashSet};
 use std::sync::Arc;
 
-fn decompose_mixed_glyphs(glyphs: &mut Vec<glyf::Glyph>, names: &[String]) {
-    // Decompose mixed.
-    let mut to_replace: Vec<(usize, glyf::Glyph)> = vec![];
-    for (id, glyph) in glyphs.iter().enumerate() {
-        if !glyph.components.is_empty() && !glyph.contours.is_empty() {
-            log::info!("Decomposed mixed glyph {:?}", names[id]);
-            to_replace.push((id, glyph.decompose(&glyphs)));
+fn decomposed_components(glyph: &Glyph, glyphset: &Layer) -> Vec<Contour> {
+    let mut contours = Vec::new();
+
+    let mut stack: Vec<(&Component, Affine)> = Vec::new();
+
+    for component in &glyph.components {
+        stack.push((component, component.transform.into()));
+
+        while let Some((component, transform)) = stack.pop() {
+            let new_outline = match glyphset.get_glyph(&component.base) {
+                Some(g) => g,
+                None => continue,
+            };
+
+            for contour in &new_outline.contours {
+                let mut decomposed_contour = Contour::default();
+                for point in &contour.points {
+                    let new_point = transform * Point::new(point.x as f64, point.y as f64);
+                    decomposed_contour.points.push(ContourPoint::new(
+                        new_point.x as f32,
+                        new_point.y as f32,
+                        point.typ.clone(),
+                        point.smooth,
+                        point.name.clone(),
+                        None,
+                        None,
+                    ))
+                }
+                contours.push(decomposed_contour);
+            }
+
+            for new_component in new_outline.components.iter().rev() {
+                let new_transform: Affine = new_component.transform.into();
+                stack.push((new_component, transform * new_transform));
+            }
         }
     }
-    for (id, glyph) in to_replace {
-        glyphs[id] = glyph;
+
+    contours
+}
+
+fn decompose_mixed_glyphs(ufo: &mut norad::Font) {
+    let layer = ufo.default_layer_mut();
+    let mut decomposed: BTreeMap<String, Vec<norad::Contour>> = BTreeMap::new();
+    for glif in layer.iter() {
+        decomposed.insert(glif.name.to_string(), decomposed_components(glif, layer));
+    }
+    for glif in layer.iter_mut() {
+        if glif.components.is_empty() || glif.contours.is_empty() {
+            continue;
+        }
+        if let Some(contours) = decomposed.get(&glif.name.to_string()) {
+            glif.contours.extend(contours.clone());
+            glif.components.clear();
+            log::info!("Decomposed mixed glyph {:?}", glif.name);
+        }
     }
 }
 
@@ -59,7 +106,8 @@ fn get_glyph_names_and_mapping(
     names
 }
 
-pub fn build_font(ufo: norad::Font, include: Option<HashSet<String>>) -> font::Font {
+pub fn build_font(mut ufo: norad::Font, include: Option<HashSet<String>>) -> font::Font {
+    decompose_mixed_glyphs(&mut ufo);
     let layer = ufo.default_layer();
     let info = ufo.font_info.as_ref().unwrap();
 
@@ -67,8 +115,8 @@ pub fn build_font(ufo: norad::Font, include: Option<HashSet<String>>) -> font::F
     let mut name_to_id: BTreeMap<String, u16> = BTreeMap::new();
 
     let names = get_glyph_names_and_mapping(&layer, &mut mapping, &mut name_to_id, &include);
-    let glifs: Vec<Arc<norad::Glyph>> = layer.iter_contents().collect();
-    let (mut glyphs, mut metrics): (Vec<glyf::Glyph>, Vec<hmtx::Metric>) = glifs
+    let glifs: Vec<&Arc<norad::Glyph>> = layer.iter().collect();
+    let (glyphs, mut metrics): (Vec<glyf::Glyph>, Vec<hmtx::Metric>) = glifs
         .iter()
         .filter(|g| include.is_none() || include.as_ref().unwrap().contains(&g.name.to_string()))
         .map({
@@ -80,17 +128,19 @@ pub fn build_font(ufo: norad::Font, include: Option<HashSet<String>>) -> font::F
             }
         })
         .unzip();
-    decompose_mixed_glyphs(&mut glyphs, &names);
     let glyf_table = form_glyf_and_fix_bounds(glyphs, &mut metrics);
     fill_tables(info, glyf_table, metrics, names, mapping)
 }
 
 pub fn build_fonts(
     default_master: usize,
-    fonts: Vec<norad::Font>,
+    mut fonts: Vec<norad::Font>,
     variation_model: VariationModel,
     include: Option<HashSet<String>>,
 ) -> font::Font {
+    for f in fonts.iter_mut() {
+        decompose_mixed_glyphs(f);
+    }
     let layer = fonts[default_master].default_layer();
     let info = fonts[default_master].font_info.as_ref().unwrap();
     let mut mapping: BTreeMap<u32, u16> = BTreeMap::new();
@@ -98,7 +148,7 @@ pub fn build_fonts(
 
     let names = get_glyph_names_and_mapping(&layer, &mut mapping, &mut name_to_id, &include);
 
-    let glifs: Vec<Arc<norad::Glyph>> = layer.iter_contents().collect();
+    let glifs: Vec<&Arc<norad::Glyph>> = layer.iter().collect();
 
     let mut glyphs: Vec<glyf::Glyph> = vec![];
     let mut metrics: Vec<hmtx::Metric> = vec![];
@@ -129,8 +179,6 @@ pub fn build_fonts(
         variations.push(variation);
     }
 
-    // XXX, you can't do this here.
-    decompose_mixed_glyphs(&mut glyphs, &names);
     let glyf_table = form_glyf_and_fix_bounds(glyphs, &mut metrics);
     let mut font = fill_tables(info, glyf_table, metrics, names, mapping);
     let gvar_table = fonttools::gvar::gvar { variations };
