f39b1438dd3c7d67a565882d517fe45fd6e780e4
Simon Cozens <simon@simon-cozens.org>
2021-05-26 16:13:10 +0100

    Serialize ligatures

--- src/layout/gsub4.rs
+++ src/layout/gsub4.rs
@@ -19,6 +19,7 @@ tables!(
 );
 
 #[allow(non_camel_case_types, non_snake_case)]
+#[derive(Debug, PartialEq)]
 struct Ligature {
     ligatureGlyph: uint16,
     componentGlyphIDs: Vec<uint16>,
@@ -68,6 +69,9 @@ deserialize_visitor!(
     {
         let lig_glyph = read_field!(seq, uint16, "a ligature glyph");
         let comp_count = read_field!(seq, uint16, "a component count");
+        if comp_count < 1 {
+            return Err(serde::de::Error::custom("Overflow in ligature component"));
+        }
         let component_glyph_ids: Vec<uint16> =
             read_field_counted!(seq, comp_count - 1, "component glyph IDs");
         Ok(Ligature {
@@ -77,44 +81,121 @@ deserialize_visitor!(
     }
 );
 
-// impl Serialize for LigatureSubst {
-//     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-//     where
-//         S: Serializer,
-//     {
-//         let mut seq = serializer.serialize_seq(None)?;
-//         seq.serialize_element(&1_u16)?;
+impl Serialize for Ligature {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let mut seq = serializer.serialize_seq(None)?;
+        seq.serialize_element(&self.ligatureGlyph)?;
+        seq.serialize_element(&(self.componentGlyphIDs.len() as uint16 + 1))?;
+        seq.serialize_element(&self.componentGlyphIDs)?;
+        seq.end()
+    }
+}
 
-//         let coverage = Coverage {
-//             glyphs: self.mapping.keys().map(|s| *(s.first().unwrap())).collect(),
-//         };
-//         let sequence_count = self.mapping.len() as uint16;
-//         let mut sequences: BTreeMap<Vec<uint16>, uint16> = BTreeMap::new();
-//         let mut offsets: Vec<uint16> = vec![];
-//         let mut seq_offset = 6 + sequence_count * 2;
-//         let serialized_cov = otspec::ser::to_bytes(&coverage).unwrap();
-//         seq.serialize_element(&seq_offset)?;
-//         seq_offset += serialized_cov.len() as uint16;
+impl Serialize for LigatureSubst {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let mut seq = serializer.serialize_seq(None)?;
+        seq.serialize_element(&1_u16)?;
 
-//         let mut sequences_ser: Vec<u8> = vec![];
-//         for right in self.mapping.values() {
-//             if sequences.contains_key(right) {
-//                 offsets.push(*sequences.get(right).unwrap());
-//             } else {
-//                 let sequence = Sequence {
-//                     substituteGlyphIDs: right.to_vec(),
-//                 };
-//                 let serialized = otspec::ser::to_bytes(&sequence).unwrap();
-//                 sequences.insert(right.to_vec(), seq_offset);
-//                 offsets.push(seq_offset);
-//                 seq_offset += serialized.len() as u16;
-//                 sequences_ser.extend(serialized);
-//             }
-//         }
-//         seq.serialize_element(&sequence_count)?;
-//         seq.serialize_element(&offsets)?;
-//         seq.serialize_element(&coverage)?;
-//         seq.serialize_element(&sequences_ser)?;
-//         seq.end()
-//     }
-// }
+        // Split the map by covered first glyph
+        let mut split_map: BTreeMap<u16, Vec<Vec<u16>>> = BTreeMap::new();
+        for left in self.mapping.keys() {
+            let covered = left.first().unwrap();
+            split_map
+                .entry(*covered)
+                .or_insert_with(std::vec::Vec::new)
+                .push(left.clone());
+        }
+        println!("Split map {:?}", split_map);
+
+        let coverage = Coverage {
+            glyphs: split_map.keys().copied().collect(),
+        };
+        let ligature_set_count = coverage.glyphs.len() as uint16;
+        println!("Ligature set count = {:?}", ligature_set_count);
+        let mut offsets: Vec<uint16> = vec![];
+        let mut seq_offset = 6 + ligature_set_count * 2;
+        let serialized_cov = otspec::ser::to_bytes(&coverage).unwrap();
+        println!("Offset to coverage = {:?}", seq_offset);
+        seq.serialize_element(&seq_offset)?;
+        seq_offset += serialized_cov.len() as uint16;
+        let mut output: Vec<u8> = vec![];
+
+        for first in &coverage.glyphs {
+            println!("For covered glyph {:?}", first);
+            println!("Offset: {:?}", seq_offset + output.len() as u16);
+            offsets.push(seq_offset + output.len() as u16);
+            let mut ls = LigatureSet {
+                ligatureOffsets: vec![],
+            };
+            let relevant_keys = split_map.get(&first).unwrap();
+            let ligatures: Vec<Ligature> = relevant_keys
+                .iter()
+                .map(|k| Ligature {
+                    ligatureGlyph: *self.mapping.get(k).unwrap(),
+                    componentGlyphIDs: k[1..].to_vec(),
+                })
+                .collect();
+            println!("  Ligatures: {:?}", ligatures);
+            let mut offset = 2 + 2 * ligatures.len();
+            let mut serialized_ligatures: Vec<u8> = vec![];
+            for liga in ligatures {
+                ls.ligatureOffsets.push(offset as u16);
+                let this = otspec::ser::to_bytes(&liga).unwrap();
+                offset += this.len();
+                serialized_ligatures.extend(this);
+            }
+            println!("  Ligature set: {:?}", ls);
+            output.extend(otspec::ser::to_bytes(&ls).unwrap());
+            println!(
+                "   Serialized ligature set {:?}",
+                otspec::ser::to_bytes(&ls).unwrap()
+            );
+            println!("   Serialized Ligatures {:?}", serialized_ligatures);
+            output.extend(serialized_ligatures);
+        }
+        seq.serialize_element(&ligature_set_count)?;
+        seq.serialize_element(&offsets)?;
+        seq.serialize_element(&coverage)?;
+        seq.serialize_element(&output)?;
+        seq.end()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::iter::FromIterator;
+
+    macro_rules! btreemap {
+        ($($k:expr => $v:expr),* $(,)?) => {
+            std::collections::BTreeMap::<_, _>::from_iter(std::array::IntoIter::new([$(($k, $v),)*]))
+        };
+    }
+
+    #[test]
+    fn test_ligature_ser() {
+        let subst = LigatureSubst {
+            mapping: btreemap!(
+                vec![ 10, 20, 30] => 11,
+                vec![ 10, 20, 31] => 12,
+                vec![ 20, 30] => 21,
+                vec![ 20, 40, 50] => 22,
+            ),
+        };
+        let serialized = otspec::ser::to_bytes(&subst).unwrap();
+        assert_eq!(
+            serialized,
+            vec![
+                0, 1, 0, 10, 0, 2, 0, 18, 0, 40, 0, 1, 0, 2, 0, 10, 0, 20, 0, 2, 0, 6, 0, 14, 0,
+                11, 0, 3, 0, 20, 0, 30, 0, 12, 0, 3, 0, 20, 0, 31, 0, 2, 0, 6, 0, 12, 0, 21, 0, 2,
+                0, 30, 0, 22, 0, 3, 0, 40, 0, 50
+            ]
+        );
+    }
+}
