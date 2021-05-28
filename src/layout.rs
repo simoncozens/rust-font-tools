aa4298c8b660d77b8a6edab5c5fa001b381d7a0d
Simon Cozens <simon@simon-cozens.org>
2021-05-28 14:04:32 +0100

    Start doing layout common tables

--- src/layout.rs
+++ src/layout.rs
@@ -1,14 +1,14 @@
-/// Glyph class definition tables
-pub mod classdef;
+// /// Glyph class definition tables
+// pub mod classdef;
 /// Common tables for OpenType Layout
 pub mod common;
 /// Glyph coverage tables
 pub mod coverage;
-/// GSUB1 single substitution
-pub mod gsub1;
-/// GSUB2 multiple substitution
-pub mod gsub2;
-/// GSUB3 alternate substitution
-pub mod gsub3;
-/// GSUB4 ligature substitution
-pub mod gsub4;
+// /// GSUB1 single substitution
+// pub mod gsub1;
+// /// GSUB2 multiple substitution
+// pub mod gsub2;
+// /// GSUB3 alternate substitution
+// pub mod gsub3;
+// /// GSUB4 ligature substitution
+// pub mod gsub4;
