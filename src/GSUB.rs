d45b8063bde4f81f64ad023e4d62ab9b9e0314d7
Simon Cozens <simon@simon-cozens.org>
2021-07-08 16:06:27 +0100

    This is a hot mess but it works so I’m checking it in before I break it again

--- src/GSUB.rs
+++ src/GSUB.rs
@@ -1,8 +1,10 @@
 use crate::layout::common::*;
 use crate::layout::gsub1::SingleSubst;
+use crate::layout::gsub1::SingleSubstInternal;
 use crate::layout::gsub2::MultipleSubst;
 use crate::layout::gsub3::AlternateSubst;
 use crate::layout::gsub4::LigatureSubst;
+
 use otspec::types::*;
 use otspec::Counted;
 use otspec::{
@@ -13,25 +15,61 @@ use otspec_macros::{tables, Deserialize, Serialize};
 use std::convert::TryInto;
 
 #[allow(missing_docs, non_snake_case, non_camel_case_types)]
-#[derive(Serialize, Debug, Clone, Deserialize)]
-pub struct gsubcore {
+#[derive(Debug, Clone, Deserialize)]
+pub struct gsubcoreincoming {
     pub majorVersion: uint16,
     pub minorVersion: uint16,
     pub scriptList: Offset16<ScriptList>,
     pub featureList: Offset16<FeatureList>,
-    pub lookupList: Offset16<SubstLookupList>,
+    pub lookupList: Offset16<SubstLookupListIncoming>,
 }
 
-tables!(
-    SubstLookupList {
-        [offset_base]
-        CountedOffset16(SubstLookup) lookups
+#[allow(missing_docs, non_snake_case, non_camel_case_types)]
+#[derive(Debug, Serialize)]
+pub struct gsubcoreoutgoing {
+    pub majorVersion: uint16,
+    pub minorVersion: uint16,
+    pub scriptList: Offset16<ScriptList>,
+    pub featureList: Offset16<FeatureList>,
+    pub lookupList: Offset16<SubstLookupListOutgoing>,
+}
+
+#[allow(missing_docs, non_snake_case, non_camel_case_types)]
+#[derive(Debug, Clone, Deserialize)]
+pub struct SubstLookupListIncoming {
+    #[serde(offset_base)]
+    #[serde(with = "Counted")]
+    pub lookups: VecOffset16<SubstLookup>,
+}
+
+#[allow(missing_docs, non_snake_case, non_camel_case_types)]
+#[derive(Debug)]
+pub struct SubstLookupListOutgoing {
+    lookups: VecOffset16<LookupInternal>,
+}
+
+#[automatically_derived]
+impl otspec::Serialize for SubstLookupListOutgoing {
+    fn to_bytes(&self, data: &mut Vec<u8>) -> Result<(), otspec::SerializationError> {
+        let obj = otspec::offsetmanager::resolve_offsets(self);
+        self.to_bytes_shallow(data)?;
+        otspec::offsetmanager::resolve_offsets_and_serialize(obj, data, false)?;
+        Ok(())
     }
-
-);
-
-pub(crate) trait ToBytes {
-    fn to_bytes(&self) -> Vec<u8>;
+    fn to_bytes_shallow(&self, data: &mut Vec<u8>) -> Result<(), otspec::SerializationError> {
+        let obj = self;
+        data.put(self.lookups.0.len() as uint16)?;
+        self.lookups.0.to_bytes_shallow(data)?;
+        Ok(())
+    }
+    fn ot_binary_size(&self) -> usize {
+        2 + 2 * self.lookups.0.len()
+    }
+    fn offset_fields(&self) -> Vec<&dyn OffsetMarkerTrait> {
+        let mut v: Vec<&dyn OffsetMarkerTrait> = Vec::new();
+        v.extend(self.lookups.offset_fields());
+        v
+    }
 }
 
 /// A general substitution lookup rule, of whatever type
@@ -43,6 +81,50 @@ pub struct SubstLookup {
     pub mark_filtering_set: Option<uint16>,
     /// The concrete substitution rule.
     pub substitution: Substitution,
+}
+
+#[derive(Debug)]
+struct LookupInternal {
+    pub lookupType: uint16,
+    pub flags: LookupFlags,
+    pub subtables: Vec<Box<dyn OffsetMarkerTrait>>,
+    pub mark_filtering_set: Option<uint16>,
+}
+
+impl otspec::Serialize for LookupInternal {
+    fn to_bytes(&self, data: &mut Vec<u8>) -> Result<(), otspec::SerializationError> {
+        let obj = otspec::offsetmanager::resolve_offsets(self);
+        self.to_bytes_shallow(data)?;
+        otspec::offsetmanager::resolve_offsets_and_serialize(obj, data, false)?;
+        Ok(())
+    }
+    fn to_bytes_shallow(&self, data: &mut Vec<u8>) -> Result<(), otspec::SerializationError> {
+        let obj = self;
+        obj.lookupType.to_bytes(data)?;
+        obj.flags.to_bytes(data)?;
+        (obj.subtables.len() as uint16).to_bytes(data)?;
+        for st in &obj.subtables {
+            st.to_bytes_shallow(data)?;
+        }
+        obj.mark_filtering_set.to_bytes(data)?;
+        Ok(())
+    }
+    fn ot_binary_size(&self) -> usize {
+        self.lookupType.ot_binary_size()
+            + self.flags.ot_binary_size()
+            + 2
+            + 2 * self.subtables.len()
+            + self.mark_filtering_set.ot_binary_size()
+    }
+    fn offset_fields(&self) -> Vec<&dyn OffsetMarkerTrait> {
+        self.subtables.iter().map(|x| x.as_ref()).collect()
+    }
+}
+
+impl Clone for LookupInternal {
+    fn clone(&self) -> Self {
+        panic!("Can't clone this")
+    }
 }
 
 impl SubstLookup {
@@ -82,7 +164,7 @@ pub enum Substitution {
     ReverseChaining,
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 #[allow(clippy::upper_case_acronyms)]
 /// The Glyph Substitution table
 pub struct GSUB {
@@ -97,7 +179,7 @@ pub struct GSUB {
 
 impl Deserialize for GSUB {
     fn from_bytes(c: &mut ReaderContext) -> Result<Self, DeserializationError> {
-        let core: gsubcore = c.de()?;
+        let core: gsubcoreincoming = c.de()?;
         if core.minorVersion == 1 {
             let _featureVariationsOffset: uint16 = c.de()?;
         }
@@ -143,6 +225,37 @@ impl Deserialize for GSUB {
     }
 }
 
+impl From<&GSUB> for gsubcoreoutgoing {
+    fn from(val: &GSUB) -> Self {
+        let substlookuplist: SubstLookupListOutgoing = SubstLookupListOutgoing {
+            lookups: VecOffset16(val.lookups.iter().map(|x| Offset16::to(x.into())).collect()),
+        };
+        let featurelist: FeatureList = FeatureList {
+            featureRecords: val
+                .features
+                .iter()
+                .map(|f| {
+                    let indices: Vec<uint16> = f.1.iter().map(|x| *x as uint16).collect();
+                    FeatureRecord {
+                        featureTag: f.0,
+                        feature: Offset16::to(FeatureTable {
+                            featureParamsOffset: 0,
+                            lookupListIndices: indices,
+                        }),
+                    }
+                })
+                .collect(),
+        };
+        gsubcoreoutgoing {
+            majorVersion: 1,
+            minorVersion: 0,
+            scriptList: Offset16::to(val.scripts.clone()),
+            featureList: Offset16::to(featurelist),
+            lookupList: Offset16::to(substlookuplist),
+        }
+    }
+}
+
 impl Deserialize for SubstLookup {
     fn from_bytes(c: &mut ReaderContext) -> Result<Self, DeserializationError> {
         c.push();
@@ -178,37 +291,44 @@ impl Deserialize for SubstLookup {
     }
 }
 
-impl Serialize for SubstLookup {
-    fn to_bytes(&self, data: &mut Vec<u8>) -> Result<(), SerializationError> {
-        let lut = self.lookup_type();
-        data.put(lut)?;
-        data.put(self.flags)?;
-        match &self.substitution {
+impl<'a> From<&SubstLookup> for LookupInternal {
+    fn from(val: &SubstLookup) -> Self {
+        let subtables: Vec<Box<dyn OffsetMarkerTrait>> = match &val.substitution.clone() {
             Substitution::Single(subs) => {
-                data.put(subs.len() as uint16)?;
-                data.put(subs)?;
+                let mut v: Vec<Box<dyn OffsetMarkerTrait>> = vec![];
+                for s in subs {
+                    let si: SingleSubstInternal = s.into();
+                    v.push(Box::new(Offset16::to(si)));
+                }
+                v
             }
+            // Substitution::Multiple(subs) => subs.offset_fields(),
+            // Substitution::Alternate(subs) => subs.offset_fields(),
+            // Substitution::Ligature(subs) => subs.offset_fields(),
             _ => unimplemented!(),
-        }
-        // data.put(Counted(self.subtables))?;
-        if self.flags.contains(LookupFlags::USE_MARK_FILTERING_SET) {
-            data.put(self.mark_filtering_set.unwrap_or(0))?;
-        }
-        Ok(())
-    }
+        };
 
-    fn offset_fields(&self) -> Vec<&dyn OffsetMarkerTrait> {
-        match &self.substitution {
-            Substitution::Single(subs) => subs.offset_fields(),
-            Substitution::Multiple(subs) => subs.offset_fields(),
-            _ => unimplemented!(),
+        LookupInternal {
+            flags: val.flags,
+            lookupType: val.lookup_type(),
+            mark_filtering_set: val.mark_filtering_set,
+            subtables,
         }
+    }
+}
+
+impl Serialize for GSUB {
+    fn to_bytes(&self, data: &mut Vec<u8>) -> Result<(), SerializationError> {
+        let gsc: gsubcoreoutgoing = self.into();
+        gsc.to_bytes(data)
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use chrono::Offset;
+    use otspec::offsetmanager::OffsetManager;
     use pretty_assertions::assert_eq;
     use std::collections::BTreeMap;
     use std::iter::FromIterator;
@@ -319,5 +439,67 @@ mod tests {
         };
         let deserialized: GSUB = otspec::de::from_bytes(&binary_gsub).unwrap();
         assert_eq!(deserialized, expected);
+    }
+
+    #[test]
+    fn test_very_simple_gsub_ser() {
+        let gsub = GSUB {
+            lookups: vec![SubstLookup {
+                flags: LookupFlags::empty(),
+                mark_filtering_set: None,
+                substitution: Substitution::Single(vec![SingleSubst {
+                    mapping: btreemap!(386 => 459),
+                }]),
+            }],
+            scripts: ScriptList {
+                scripts: hashmap!(*b"DFLT" => Script {
+                    default_language_system: Some(
+                        LanguageSystem {
+                            required_feature: None,
+                            feature_indices: vec![
+                                0,
+                           ],
+                        },
+                    ),
+                    language_systems: BTreeMap::new()
+                }),
+            },
+            features: vec![(*b"liga", vec![0], None)],
+        };
+
+        let serialized = otspec::ser::to_bytes(&gsub).unwrap();
+
+        assert_eq!(
+            serialized,
+            vec![
+                /* 00 */ 0x00, 0x01, 0x00, 0x00, // version 1.0
+                /* 04 */ 0x00, 0x0a, // offset to script list
+                /* 06 */ 0x00, 0x1e, // offset to feature list
+                /* 08 */ 0x00, 0x2c, // offset to lookup list
+                /* 0a */ 0x00, 0x01, // ScriptList: script count
+                /* 0c */ 0x44, 0x46, 0x4c, 0x54, // script tag
+                /* 10 */ 0x00, 0x08, // script table offset: 0x08 + 0x0a = 0x12
+                /* 12 */ 0x00,
+                0x04, // ScriptTable: default langsys offset = 0x12 + 0x14 = 0x16
+                /* 14 */ 0x00, 0x00, // langsys count
+                /* 16 */ 0x00, 0x00, // LangSys table: lookupOrderOffset
+                /* 18 */ 0xff, 0xff, // required feature index
+                /* 1a */ 0x00, 0x01, // feature index count
+                /* 1c */ 0x00, 0x00, // feature index = 0
+                /* 1e */ 0x00, 0x01, // Feature list: Feature count = 1
+                /* 20 */ 0x6c, 0x69, 0x67, 0x61, // Tag: liga
+                /* 24 */ 0x00, 0x08, // Offset 1e + 08 = 26
+                /* 26 */ 0x00, 0x00, // Feature table; featureParamsOffset
+                /* 28 */ 0x00, 0x01, // lookupIndexCount
+                /* 2a */ 0x00, 0x00, // lookup index = 0
+                /* 2c */ 0x00, 0x01, // Lookup list: lookupCount = 1
+                /* 2e */ 0x00, 0x04, // Offset to first lookup = 2c+04 = 30
+                /* 30 */ 0x00, 0x01, // Lookup table Lookup type
+                /* 32 */ 0x00, 0x00, // lookup flag
+                /* 34 */ 0x00, 0x01, // subtable count
+                /* 36 */ 0x00, 0x08, // subtable offset 30 + 08 = 38
+                0x00, 0x01, 0x00, 0x06, 0x00, 0x49, 0x00, 0x01, 0x00, 0x01, 0x01, 0x82
+            ]
+        );
     }
 }
