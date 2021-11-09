393b7804bdb2a456b8f73695a2ee7e8d05f05dea
Simon Cozens <simon@simon-cozens.org>
2021-11-09 15:32:22 +0000

    With this, we can deserialize GPOS into our high-level format *using* the lower-level format!

--- src/tables/GPOS.rs
+++ src/tables/GPOS.rs
@@ -1,49 +1,27 @@
-use crate::layout::common::{Lookup, ScriptList, GPOSGSUB};
+use crate::convert_outgoing_subtables;
+use crate::layout::common::{Lookup, GPOSGSUB};
 use crate::layout::contextual::{ChainedSequenceContext, SequenceContext};
 use crate::layout::gpos1::SinglePos;
 use crate::layout::gpos2::PairPos;
 use crate::layout::gpos3::CursivePos;
 use crate::layout::gpos4::MarkBasePos;
-use crate::{convert_outgoing_subtables, deserialize_lookup_match};
 use otspec::layout::common::{
     gsubgpos as gsubgposoutgoing, FeatureList, FeatureRecord, FeatureTable,
-    Lookup as LookupInternal, LookupFlags, LookupList as LookupListOutgoing,
+    Lookup as LookupInternal, LookupList as LookupListOutgoing,
 };
+use otspec::tables::GPOS::{GPOSSubtable, GPOS10, GPOS11};
 
 // use crate::layout::gpos5::{MarkLigPos, MarkLigPosFormat1};
 // use crate::layout::gpos6::{MarkMarkPos, MarkMarkPosFormat1};
 use otspec::types::*;
 use otspec::{
-    Counted, DeserializationError, Deserialize, Deserializer, ReaderContext, SerializationError,
-    Serialize,
+    DeserializationError, Deserialize, Deserializer, ReaderContext, SerializationError, Serialize,
 };
-use otspec_macros::Deserialize;
 use std::convert::TryInto;
 
 /// The 'GPOS' OpenType tag.
 pub const TAG: Tag = crate::tag!("GPOS");
 
-impl Lookup<Positioning> {
-    /// Return the integer GPOS lookup type for this lookup
-    pub fn lookup_type(&self) -> u16 {
-        match self.rule {
-            Positioning::Single(_) => 1,
-            Positioning::Pair(_) => 2,
-            Positioning::Cursive(_) => 3,
-            Positioning::MarkToBase(_) => 4,
-            Positioning::MarkToLig => 5,
-            Positioning::MarkToMark => 6,
-            Positioning::Contextual(_) => 7,
-            Positioning::ChainedContextual(_) => 8,
-            Positioning::Extension => 9,
-        }
-    }
-
-    /// Add subtable break
-    pub fn add_subtable_break(&mut self) {
-        self.rule.add_subtable_break()
-    }
-}
 /// A container which represents a generic positioning rule
 ///
 /// Each rule is expressed as a vector of subtables.
@@ -95,154 +73,125 @@ impl Positioning {
 pub type GPOS = GPOSGSUB<Positioning>;
 
 impl Deserialize for GPOS {
+    // It's possible we're going to need to know more context about this.
     fn from_bytes(c: &mut ReaderContext) -> Result<Self, DeserializationError> {
-        #[derive(Debug, Deserialize)]
-        struct RawLookupList {
-            #[otspec(offset_base)]
-            #[otspec(with = "Counted")]
-            pub lookups: VecOffset16<Lookup<Positioning>>,
+        match c.peek(4)? {
+            [0x00, 0x01, 0x00, 0x00] => {
+                let internal: GPOS10 = c.de()?;
+                Ok(internal.into())
+            }
+            // [0x00, 0x01, 0x00, 0x01] => {
+            //     let internal: GPOS11 = c.de()?;
+            //     Ok(internal.into())
+            // }
+            _ => Err(DeserializationError(
+                "Invalid GPOS table version".to_string(),
+            )),
         }
+    }
+}
 
-        #[derive(Deserialize)]
-        struct GposCore {
-            #[allow(dead_code)]
-            majorVersion: uint16,
-            minorVersion: uint16,
-            scriptList: Offset16<ScriptList>,
-            featureList: Offset16<FeatureList>,
-            lookupList: Offset16<RawLookupList>,
-        }
-
-        let core: GposCore = c.de()?;
-        if core.minorVersion == 1 {
-            let _feature_variations_offset: uint16 = c.de()?;
-        }
-        let scripts: ScriptList = core
-            .scriptList
-            .link
-            .ok_or_else(|| DeserializationError("Bad script list in GPOS table".to_string()))?;
-        let lookups: Vec<Lookup<Positioning>> = core
-            .lookupList
-            .link
-            .ok_or_else(|| DeserializationError("Bad lookup list in GPOS table".to_string()))?
-            .lookups
-            .try_into()?;
-        let feature_records = core
-            .featureList
-            .link
-            .ok_or_else(|| DeserializationError("Bad feature list in GPOS table".to_string()))?
-            .featureRecords;
-        let mut features = vec![];
-        for f in feature_records.iter() {
-            let tag = f.featureTag;
-            let table = f
-                .feature
-                .link
-                .as_ref()
-                .ok_or_else(|| DeserializationError("Bad feature in GPOS table".to_string()))?;
-            features.push((
-                tag,
-                table
-                    .lookupListIndices
+impl Into<GPOS> for GPOS10 {
+    fn into(self) -> GPOS {
+        let lookup_list_lowlevel = self.lookupList.link.unwrap_or_default();
+        let mut lookups: Vec<Lookup<Positioning>> = vec![];
+        for lookup_off in lookup_list_lowlevel.lookups.v {
+            if let Some(lookup_lowlevel) = lookup_off.link {
+                let subtables: Vec<GPOSSubtable> = lookup_lowlevel
+                    .subtables
+                    .v
                     .iter()
-                    .map(|x| *x as usize)
-                    .collect(),
-                None,
-            ));
-        }
+                    .map(|x| x.link.clone())
+                    .flatten()
+                    .collect();
+                let theirs = match lookup_lowlevel.lookupType {
+                    1 => Positioning::Single(subtables.into_iter().map(SinglePos::from).collect()),
+                    _ => unimplemented!(),
+                };
 
-        Ok(GPOS {
+                let lookup_highlevel: Lookup<Positioning> = Lookup {
+                    flags: lookup_lowlevel.lookupFlag,
+                    mark_filtering_set: lookup_lowlevel.markFilteringSet,
+                    rule: theirs,
+                };
+                lookups.push(lookup_highlevel)
+            }
+        }
+        GPOS {
             lookups,
-            scripts,
-            features,
-        })
-    }
-}
-
-impl From<&GPOS> for gsubgposoutgoing {
-    fn from(val: &GPOS) -> Self {
-        let substlookuplist: LookupListOutgoing = LookupListOutgoing {
-            lookups: VecOffset16 {
-                v: val.lookups.iter().map(|x| Offset16::to(x.into())).collect(),
-            },
-        };
-        let featurelist: FeatureList = FeatureList {
-            featureRecords: val
-                .features
-                .iter()
-                .map(|f| {
-                    let indices: Vec<uint16> = f.1.iter().map(|x| *x as uint16).collect();
-                    FeatureRecord {
-                        featureTag: f.0,
-                        feature: Offset16::to(FeatureTable {
-                            featureParamsOffset: 0,
-                            lookupListIndices: indices,
-                        }),
-                    }
-                })
-                .collect(),
-        };
-        gsubgposoutgoing {
-            majorVersion: 1,
-            minorVersion: 0,
-            scriptList: Offset16::to((&val.scripts).into()),
-            featureList: Offset16::to(featurelist),
-            lookupList: Offset16::to(substlookuplist),
+            scripts: self.scriptList.link.unwrap_or_default().into(),
+            features: self.featureList.link.unwrap_or_default().into(),
         }
     }
 }
 
-impl Deserialize for Lookup<Positioning> {
-    fn from_bytes(c: &mut ReaderContext) -> Result<Self, DeserializationError> {
-        c.push();
-        let lookup_type: uint16 = c.de()?;
-        let lookup_flag: LookupFlags = c.de()?;
-        let rule = deserialize_lookup_match!(
-            lookup_type,
-            c,
-            (1, SinglePos, Positioning::Single),
-            (2, PairPos, Positioning::Pair),
-            (3, CursivePos, Positioning::Cursive),
-            (4, MarkBasePos, Positioning::MarkToBase),
-            // (5, MarkLigPos, Positioning::MarkToLig),
-            // (6, MarkMarkPos, Positioning::MarkToMark),
-            (7, SequenceContext, Positioning::Contextual),
-            (8, ChainedSequenceContext, Positioning::ChainedContextual),
-        );
+// Unneeded, for reference
+// impl<'a> From<&Lookup<Positioning>> for LookupInternal {
+//     fn from(val: &Lookup<Positioning>) -> Self {
+//         let subtables: Vec<Box<dyn OffsetMarkerTrait>> = convert_outgoing_subtables!(
+//             val.rule.clone(),
+//             (Positioning::Single, SinglePosInternal),
+//             (Positioning::Pair, PairPosInternal),
+//             (Positioning::Cursive, CursivePosFormat1),
+//             (Positioning::MarkToBase, MarkBasePosFormat1),
+//             // (Positioning::MarkToLig, MarkLigPosFormat1),
+//             // (Positioning::MarkToMark, MarkMarkPosFormat1),
+//         );
 
-        c.pop();
-        Ok(Lookup {
-            flags: lookup_flag,
-            mark_filtering_set: None,
-            rule,
-        })
-    }
-}
+//         LookupInternal {
+//             flags: val.flags,
+//             lookupType: val.lookup_type(),
+//             mark_filtering_set: val.mark_filtering_set,
+//             subtables,
+//         }
+//     }
+// }
 
-impl<'a> From<&Lookup<Positioning>> for LookupInternal {
-    fn from(val: &Lookup<Positioning>) -> Self {
-        let subtables: Vec<Box<dyn OffsetMarkerTrait>> = convert_outgoing_subtables!(
-            val.rule.clone(),
-            (Positioning::Single, SinglePosInternal),
-            (Positioning::Pair, PairPosInternal),
-            (Positioning::Cursive, CursivePosFormat1),
-            (Positioning::MarkToBase, MarkBasePosFormat1),
-            // (Positioning::MarkToLig, MarkLigPosFormat1),
-            // (Positioning::MarkToMark, MarkMarkPosFormat1),
-        );
-
-        LookupInternal {
-            flags: val.flags,
-            lookupType: val.lookup_type(),
-            mark_filtering_set: val.mark_filtering_set,
-            subtables,
-        }
-    }
-}
+// Will be needed soon
 
 impl Serialize for GPOS {
-    fn to_bytes(&self, data: &mut Vec<u8>) -> Result<(), SerializationError> {
-        let gsc: gsubgposoutgoing = self.into();
-        gsc.to_bytes(data)
+    fn to_bytes(&self, _data: &mut Vec<u8>) -> Result<(), SerializationError> {
+        unimplemented!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_gpos1_highlevel_de() {
+        let binary_gpos = vec![
+            0x00, 0x01, 0x00, 0x00, // GPOS 1.0
+            0x00, 0x0a, // scriptlist offset
+            0x00, 0x1e, // featurelist offset
+            0x00, 0x2c, // lookuplist offset
+            /* 0x0a */ 0x00, 0x01, // ScriptList.scriptCount
+            0x44, 0x46, 0x4c, 0x54, // ScriptRecord.scriptTag = DFLT
+            0x00, 0x08, // ScriptRecord.scriptOffset
+            0x00, 0x04, // Script.defaultLangSysOffset
+            0x00, 0x00, // Script.langSysCount
+            0x00, 0x00, // LangSys.lookupOrderOffset
+            0xff, 0xff, // LangSys.requiredFeatureIndex
+            0x00, 0x01, // LangSys.featureIndexCount
+            0x00, 0x00, // LangSys.featureIndices
+            /* 0x1e */ 0x00, 0x01, // FeatureList.featureCount
+            0x6b, 0x65, 0x72, 0x6e, //FeatureRecord.featureTag = kern
+            0x00, 0x08, // FeatureRecord.featureOffset
+            0x00, 0x00, // Feature.featureParamsOffset
+            0x00, 0x01, // Feature.lookupIndexCount
+            0x00, 0x00, // Feature.lookupListIndices
+            /* 0x2c */ 0x00, 0x01, // LookupList.lookupCount
+            0x00, 0x04, // LookupList.lookupOffsets
+            0x00, 0x01, // Lookup.lookupType
+            0x00, 0x00, // Lookup.lookupFlags
+            0x00, 0x01, // Lookup.subtableCount
+            0x00, 0x08, // Lookup.subtableOffsets
+            0x00, 0x01, 0x00, 0x08, 0x00, 0x04, 0x00, 0x23, 0x00, 0x01, 0x00, 0x03, 0x00, 0x25,
+            0x00, 0x30, 0x00, 0x32,
+        ];
+        let gpos: GPOS = otspec::de::from_bytes(&binary_gpos).unwrap();
+        println!("{:#?}", gpos);
+        panic!();
     }
 }
