dacb0051162b04586d5a91da1b89c47dd9925eb2
Simon Cozens <simon@simon-cozens.org>
2022-05-07 22:37:01 +0100

    Parallelize UFO loading

--- src/convertors/designspace.rs
+++ src/convertors/designspace.rs
@@ -1,3 +1,7 @@
+use crate::glyph::GlyphList;
+use crate::Layer;
+use designspace::Source;
+use rayon::prelude::*;
 use std::fs::File;
 use std::path::PathBuf;
 
@@ -42,7 +46,18 @@ pub fn load(path: PathBuf) -> Result<Font, BabelfontError> {
         }
     })?;
     load_glyphs(&mut font, &default_ufo);
-    load_masters(&mut font, &ds, relative)?;
+    let res: Vec<(Master, Vec<Vec<Layer>>)> = ds
+        .sources
+        .source
+        .par_iter()
+        .filter_map(|source| load_master(&font.glyphs, &ds, &source, relative).ok())
+        .collect();
+    for (master, mut layerset) in res {
+        font.masters.push(master);
+        for (g, mut l) in font.glyphs.iter_mut().zip(layerset.iter_mut()) {
+            g.layers.append(&mut l);
+        }
+    }
     let info = default_ufo.font_info;
     load_font_info(&mut font, &info);
     font.features = Some(default_ufo.features);
@@ -66,52 +81,55 @@ pub(crate) fn load_instances(_font: &mut Font, _instances: &[DSInstance]) {
     // unimplemented!()
 }
 
-fn load_masters(
-    font: &mut Font,
+fn load_master(
+    glyphs: &GlyphList,
     ds: &Designspace,
+    source: &Source,
     relative: Option<&std::path::Path>,
-) -> Result<(), BabelfontError> {
-    for source in &ds.sources.source {
-        let location = Location(
-            ds.axes
-                .axis
-                .iter()
-                .map(|x| x.tag.clone())
-                .zip(ds.location_to_tuple(&source.location))
-                .collect(),
-        );
-        let required_layer = &source.layer;
-        let uuid = Uuid::new_v4().to_string();
+) -> Result<(Master, Vec<Vec<Layer>>), BabelfontError> {
+    let location = Location(
+        ds.axes
+            .axis
+            .iter()
+            .map(|x| x.tag.clone())
+            .zip(ds.location_to_tuple(&source.location))
+            .collect(),
+    );
+    let required_layer = &source.layer;
+    let uuid = Uuid::new_v4().to_string();
 
-        let mut master = Master::new(
-            source
-                .name
-                .as_ref()
-                .unwrap_or(&"Unnamed master".to_string()),
-            source.name.as_ref().unwrap_or(&uuid),
-            location,
-        );
-        let relative_path_to_master = if let Some(r) = relative {
-            r.join(source.filename.clone())
-        } else {
-            source.filename.clone().into()
-        };
+    let mut master = Master::new(
+        source
+            .name
+            .as_ref()
+            .unwrap_or(&"Unnamed master".to_string()),
+        source.name.as_ref().unwrap_or(&uuid),
+        location,
+    );
+    let relative_path_to_master = if let Some(r) = relative {
+        r.join(source.filename.clone())
+    } else {
+        source.filename.clone().into()
+    };
 
-        let source_font =
-            norad::Font::load(relative_path_to_master).map_err(|e| BabelfontError::LoadingUFO {
-                path: source.filename.clone(),
-                orig: e,
-            })?;
-        let info = &source_font.font_info;
-        load_master_info(&mut master, info);
-        let kerning = &source_font.kerning;
-        for (left, right_dict) in kerning.iter() {
-            for (right, value) in right_dict.iter() {
-                master
-                    .kerning
-                    .insert((left.clone(), right.clone()), *value as i16);
-            }
+    let source_font =
+        norad::Font::load(relative_path_to_master).map_err(|e| BabelfontError::LoadingUFO {
+            path: source.filename.clone(),
+            orig: e,
+        })?;
+    let info = &source_font.font_info;
+    load_master_info(&mut master, info);
+    let kerning = &source_font.kerning;
+    for (left, right_dict) in kerning.iter() {
+        for (right, value) in right_dict.iter() {
+            master
+                .kerning
+                .insert((left.clone(), right.clone()), *value as i16);
         }
+    }
+    let mut bf_layer_list = vec![];
+    for g in glyphs.iter() {
+        let mut glyph_layer_list = vec![];
         for layer in source_font.iter_layers() {
             let layername = layer.name().to_string();
             // We should probably keep all layers for interchange purposes,
@@ -122,14 +140,11 @@ fn load_masters(
                 }
             }
 
-            for g in font.glyphs.iter_mut() {
-                if let Some(norad_glyph) = layer.get_glyph(g.name.as_str()) {
-                    g.layers
-                        .push(norad_glyph_to_babelfont_layer(norad_glyph, &master.id))
-                }
+            if let Some(norad_glyph) = layer.get_glyph(g.name.as_str()) {
+                glyph_layer_list.push(norad_glyph_to_babelfont_layer(norad_glyph, &master.id))
             }
         }
-        font.masters.push(master);
+        bf_layer_list.push(glyph_layer_list)
     }
-    Ok(())
+    Ok((master, bf_layer_list))
 }
