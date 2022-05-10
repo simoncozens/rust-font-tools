9aa2b658a21dff936da0c67d8cf06e6d51524b44
Simon Cozens <simon@simon-cozens.org>
2022-05-10 14:51:22 +0100

    Hopefully easier-to-follow glyph builder

--- src/glyph.rs
+++ src/glyph.rs
@@ -3,16 +3,254 @@ use fonttools::otvar::VariationModel;
 use fonttools::tables::glyf;
 use fonttools::tables::gvar::{DeltaSet, GlyphVariationData};
 use kurbo::{cubics_to_quadratic_splines, BezPath, CubicBez, PathEl, PathSeg};
+use otspec::utils::is_all_the_same;
 use std::collections::BTreeMap;
+use std::mem;
 use unzip_n::unzip_n;
 
 unzip_n!(3);
 
+// "Once the data structures are laid out, the algorithms tend to fall
+// into place, and the coding is comparatively easy."
+
 type GlyphContour = Vec<Vec<glyf::Point>>;
 
-// OK, this is the trickiest portion of the project to follow.
-//
-// We are going to be converting a set of layers representing a single glyph
+#[derive(Default)]
+struct GlyphForConversion<'a> {
+    masters: Vec<Option<UnconvertedMaster<'a>>>,
+    default_master_ix: usize,
+    model: Option<&'a VariationModel>,
+    /// The glyph's name, for debugging purposes
+    glif_name: &'a str,
+}
+
+impl<'a> GlyphForConversion<'a> {
+    fn default_master(&self) -> &UnconvertedMaster<'a> {
+        self.masters[self.default_master_ix].as_ref().unwrap()
+    }
+
+    fn convert(mut self) -> GlyphReadyToGo<'a> {
+        assert!(
+            self.masters.get(self.default_master_ix).is_some(),
+            "Default master for {:} was not in master list",
+            self.glif_name
+        );
+        assert!(
+            self.masters[self.default_master_ix].is_some(),
+            "Default master for {:} was in master list but was not present",
+            self.glif_name
+        );
+
+        let mut result = GlyphReadyToGo {
+            masters: vec![],
+            default_master_ix: self.default_master_ix,
+            model: self.model,
+        };
+        /* OK, we're doing the contours of a variable font. Some of the masters
+        may be sparse, i.e. not containing a layer for this glyph. We will
+        keep the indices around for when we have to filter them out. */
+        let mut indexes_of_nonsparse_masters: Vec<usize> = vec![];
+        let mut nonsparse_masters: Vec<&UnconvertedMaster<'a>> = vec![];
+        let mut index_of_default_master_in_nonsparse_list: Option<usize> = None;
+        // Check for path count compatibility
+        let default_path_count = self.default_master().babelfont_contours.len();
+
+        for (ix, m) in self.masters.iter_mut().enumerate() {
+            if let Some(m) = m {
+                indexes_of_nonsparse_masters.push(ix);
+                nonsparse_masters.push(m);
+                result.masters.push(Some(ConvertedMaster {
+                    glyf_contours: vec![],
+                    components: (*m.components).to_vec(),
+                    width: m.width,
+                }))
+            } else {
+                result.masters.push(None);
+            }
+            if ix == self.default_master_ix {
+                index_of_default_master_in_nonsparse_list =
+                    Some(indexes_of_nonsparse_masters.len() - 1);
+            }
+        }
+        let path_lengths = nonsparse_masters.iter().map(|x| x.babelfont_contours.len());
+        if !is_all_the_same(path_lengths) {
+            let lengths: Vec<usize> = nonsparse_masters
+                .iter()
+                .map(|x| x.babelfont_contours.len())
+                .collect();
+            log::error!(
+                "Incompatible path count in glyph {:}: {:?}",
+                self.glif_name,
+                lengths
+            );
+            return self.into_nonvariable().convert();
+        }
+
+        // Convert path by path
+        for index in 0..default_path_count {
+            let all_contours: Vec<&babelfont::Path> = nonsparse_masters
+                .iter()
+                .map(|x| *x.babelfont_contours.get(index).unwrap())
+                .collect();
+            let all_glyf_contours = babelfont_contours_to_glyf_contours(
+                index,
+                all_contours,
+                index_of_default_master_in_nonsparse_list.unwrap(),
+                self.glif_name,
+            );
+            for (finished_contour, &master_id) in all_glyf_contours
+                .iter()
+                .zip(indexes_of_nonsparse_masters.iter())
+            {
+                assert!(result.masters[master_id].is_some());
+                result.masters[master_id]
+                    .as_mut()
+                    .unwrap()
+                    .glyf_contours
+                    .push(finished_contour.clone());
+            }
+        }
+        result
+    }
+
+    /// Drop all variation masters
+    fn into_nonvariable(mut self) -> Self {
+        let default_master: Option<UnconvertedMaster> =
+            mem::replace(&mut self.masters[self.default_master_ix], None);
+        GlyphForConversion {
+            masters: vec![default_master],
+            default_master_ix: 0,
+            model: None,
+            glif_name: self.glif_name,
+        }
+    }
+}
+
+#[derive(Default, Debug)]
+struct UnconvertedMaster<'a> {
+    babelfont_contours: Vec<&'a babelfont::Path>,
+    components: Vec<glyf::Component>,
+    width: i32,
+}
+
+#[derive(Default)]
+struct GlyphReadyToGo<'a> {
+    masters: Vec<Option<ConvertedMaster>>,
+    default_master_ix: usize,
+    model: Option<&'a VariationModel>,
+}
+
+impl<'a> GlyphReadyToGo<'a> {
+    fn into_glyph(mut self) -> glyf::Glyph {
+        // We just build a glyph from the default master
+        // There are invariants around the fact that when we built this thing we
+        // have a default layer.
+        let l: Option<ConvertedMaster> =
+            mem::replace(&mut self.masters[self.default_master_ix], None);
+        l.unwrap().into_glyph()
+    }
+    fn variation_data(&self) -> Option<GlyphVariationData> {
+        self.model?;
+        let model = self.model.as_ref().unwrap();
+        let mut deltasets: Vec<DeltaSet> = vec![];
+        let all_coords: Vec<Option<ndarray::Array2<f32>>> = self
+            .masters
+            .iter()
+            .map(|o| o.as_ref().map(|m| m.gvar_coords()))
+            .collect();
+        // The model takes Vec<T> T:Sub, and ndarray::Array2 implements Sub,
+        // so we can just send the whole vec of ndarrays to the model and get
+        // back our deltas.
+        let deltas_and_supports = model.get_deltas_and_supports(&all_coords);
+
+        for (delta, support) in deltas_and_supports.iter() {
+            // log::debug!(" Delta {:#?}", delta);
+            // log::debug!(" Support {:#?}", support);
+            if support.is_empty() {
+                continue;
+            }
+
+            // Turn the ndarray back into a vec of tuples
+            let deltas: Vec<(i16, i16)> = delta
+                .mapv(|x| x as i16)
+                .outer_iter()
+                .map(|x| (x[0], x[1]))
+                .collect();
+
+            // The variation model gives us the tents for each deltaset
+            let tuples = model
+                .axis_order
+                .iter()
+                .map(|ax| support.get(ax).unwrap_or(&(0.0, 0.0, 0.0)))
+                .copied();
+            let (start, peak, end) = tuples.into_iter().unzip_n_vec();
+
+            // And we're done
+            deltasets.push(DeltaSet {
+                peak,
+                start,
+                end,
+                deltas,
+            })
+        }
+        // log::debug!("Deltasets {:#?}", deltasets);
+        Some(GlyphVariationData { deltasets })
+    }
+}
+
+#[derive(Default)]
+struct ConvertedMaster {
+    glyf_contours: GlyphContour,
+    components: Vec<glyf::Component>,
+    width: i32,
+}
+
+impl<'a> ConvertedMaster {
+    fn gvar_coords(&self) -> ndarray::Array2<f32> {
+        let width = self.width;
+        // Flatten all points (i.e. combine all contours together) in the glyph
+        // and split up X and Y into separate arrays.
+        let (mut master_x_coords, mut master_y_coords): (Vec<f32>, Vec<f32>) = self
+            .glyf_contours
+            .iter()
+            .flatten()
+            .map(|pt| (pt.x as f32, pt.y as f32))
+            .unzip();
+
+        for c in &self.components {
+            let [_, _, _, _, x, y] = c.transformation.as_coeffs();
+            master_x_coords.push(x as f32);
+            master_y_coords.push(y as f32);
+        }
+
+        // Add the phantom points
+        master_x_coords.extend(vec![0_f32, width as f32, 0.0, 0.0]);
+        master_y_coords.extend(vec![0.0, 0.0, 0.0, 0.0]);
+
+        // Concat the X-coordinates/Y-coordinates in preparation for being
+        // reshaped into a 2d ndarray.
+        let len = master_x_coords.len();
+        master_x_coords.extend(master_y_coords);
+        ndarray::Array2::from_shape_vec((2, len), master_x_coords)
+            .unwrap()
+            .reversed_axes()
+    }
+
+    fn into_glyph(self) -> glyf::Glyph {
+        glyf::Glyph {
+            xMin: 0,
+            xMax: 0,
+            yMin: 0,
+            yMax: 0,
+            contours: self.glyf_contours,
+            instructions: vec![],
+            components: self.components,
+            overlap: false,
+        }
+    }
+}
+
+// We are going to be converting a set of masters representing a single glyph
 // at different points in the designspace into a base `glyf` table entry plus
 // `gvar` table information.
 //
@@ -23,131 +261,40 @@ pub fn layers_to_glyph(
     default_master: usize,
     // A mapping of glyph names to glyph IDs (for resolving components)
     mapping: &BTreeMap<String, u16>,
-    // The set of layers
-    layers: &[Option<&babelfont::Layer>],
-    // A variation model, which tells us where all the layers live in the
+    // The set of masters
+    masters: &[Option<&babelfont::Layer>],
+    // A variation model, which tells us where all the masters live in the
     // design space
     model: Option<&VariationModel>,
     // and the glyph's name, for debugging purposes
     glif_name: &str,
 ) -> (glyf::Glyph, Option<GlyphVariationData>) {
-    let mut glyph = glyf::Glyph {
-        xMin: 0,
-        xMax: 0,
-        yMin: 0,
-        yMax: 0,
-        contours: vec![],
-        instructions: vec![],
-        components: vec![],
-        overlap: false,
+    let mut for_conversion = GlyphForConversion {
+        masters: vec![],
+        default_master_ix: default_master,
+        model,
+        glif_name,
     };
 
-    let default_layer = layers
-        .get(default_master)
-        .expect("Couldn't index the default master into the layer list?!")
-        .expect("No glif in default master!");
-
-    /* Dispatch empty glyphs (space, etc.) straight away */
-    if !default_layer.has_components() && !default_layer.has_paths() {
-        return (glyph, None);
-    }
-
-    /* Do components */
-    for component in default_layer.components() {
-        if let Some(glyf_component) = babelfont_component_to_glyf_component(component, mapping) {
-            glyph.components.push(glyf_component);
-        }
-        // XXX Oops, we also need to compute variations on the component positions
-    }
-
-    /* Now we will do the outlines */
-
-    /* Handle the simple case of a static font. */
-    if model.is_none() {
-        for (contour_ix, contour) in default_layer.paths().enumerate() {
-            let glyph_contour =
-                babelfont_contours_to_glyf_contours(contour_ix, vec![contour], 0, glif_name)
-                    .first()
-                    .unwrap()
-                    .clone();
-            glyph.contours.push(glyph_contour);
-        }
-        return (glyph, None);
-    }
-
-    /* OK, we're doing the contours of a variable font. Some of the masters
-    may be sparse, i.e. not containing a layer for this glyph. We will
-    keep the indices around for when we have to filter them out. */
-    let indexes_of_nonsparse_masters: Vec<usize> =
-        (0..layers.len()).filter(|x| layers[*x].is_some()).collect();
-
-    let mut widths: Vec<Option<i32>> = vec![];
-    let mut contours: Vec<Option<GlyphContour>> = vec![];
-
-    for o in layers {
-        widths.push(o.and_then(|x| Some(x.width)));
-        contours.push(o.and_then(|_| Some(vec![])));
-    }
-
-    // Convert each contour in turn (across layers)
-    for (index, _) in default_layer.paths().enumerate() {
-        for o in layers {
-            // If this contour doesn't exist in a given layer, we have a problem
-            if o.is_some() && index >= o.unwrap().paths().count() {
-                log::error!("Incompatible contour count in glyph {:}", glif_name);
-                return (glyph, None);
-            }
-        }
-
-        // List of all contours across *non-sparse* layers.
-        let all_contours: Vec<&babelfont::Path> = layers
-            .iter()
-            .filter(|g| g.is_some())
-            .map(|x| x.unwrap().paths().nth(index).unwrap())
-            .collect();
-
-        // Convert them together into OT contours
-        let all_glyf_contours =
-            babelfont_contours_to_glyf_contours(index, all_contours, default_master, glif_name);
-
-        // Now we put them into their respective master
-        for (finished_contour, &master_id) in all_glyf_contours
-            .iter()
-            .zip(indexes_of_nonsparse_masters.iter())
-        {
-            assert!(contours[master_id].is_some());
-            contours[master_id]
-                .as_mut()
-                .unwrap()
-                .push(finished_contour.clone());
+    for maybe_layer in masters {
+        if let Some(layer) = maybe_layer {
+            for_conversion.masters.push(Some(UnconvertedMaster {
+                babelfont_contours: layer.paths().collect(),
+                components: layer
+                    .components()
+                    .map(|component| babelfont_component_to_glyf_component(component, mapping))
+                    .flatten()
+                    .collect(),
+                width: layer.width,
+            }));
+        } else {
+            for_conversion.masters.push(None);
         }
     }
 
-    // Now generate variations
-    if layers.len() > 1
-        && !contours.is_empty()
-        && !contours[default_master].as_ref().unwrap().is_empty()
-    {
-        // Gather all contour lengths, ensure they are the same.
-        // XXX should be caught in babelfont_contours_to_glyf_contours?
-        let lengths: Vec<usize> = contours
-            .iter()
-            .filter(|x| x.is_some())
-            .map(|g| g.as_ref().unwrap().iter().flatten().count())
-            .collect();
-        if !is_all_same(&lengths) {
-            log::warn!("Incompatible glyph: {:}, lengths: {:?}", glif_name, lengths);
-            glyph.contours = contours[default_master].as_ref().unwrap().clone();
-            return (glyph, None);
-        }
-
-        // We have everything we need
-        let deltas = compute_deltas(&contours, widths, model.unwrap());
-        glyph.contours = contours[default_master].as_ref().unwrap().clone();
-        return (glyph, Some(deltas));
-    }
-
-    (glyph, None)
+    let result: GlyphReadyToGo = for_conversion.convert();
+    let variation_data: Option<GlyphVariationData> = result.variation_data();
+    (result.into_glyph(), variation_data)
 }
 
 fn babelfont_contours_to_glyf_contours(
@@ -162,7 +309,7 @@ fn babelfont_contours_to_glyf_contours(
 
     // Which glyph this is (for error reporting)
     glif_name: &str,
-) -> Vec<Vec<glyf::Point>> {
+) -> GlyphContour {
     // Let's first get them all to kurbo elements.
     let kurbo_paths: Vec<BezPath> = paths
         .iter()
@@ -191,7 +338,7 @@ fn babelfont_contours_to_glyf_contours(
     for (el_ix, el) in default_elements.iter().enumerate() {
         match el {
             PathEl::CurveTo(_, _, _) => {
-                // Convert all the cubics to quadratics in one go, across layers
+                // Convert all the cubics to quadratics in one go, across masters
                 let all_curves: Vec<CubicBez> = kurbo_paths
                     .iter()
                     .filter_map(|x| match x.get_seg(el_ix).unwrap() {
@@ -269,79 +416,4 @@ fn babelfont_component_to_glyf_component(
         log::warn!("Couldn't find component for {:?}", component.reference);
         None
     }
-}
-
-fn compute_deltas(
-    contours: &[Option<GlyphContour>],
-    widths: Vec<Option<i32>>,
-    model: &VariationModel,
-) -> GlyphVariationData {
-    let mut deltasets: Vec<DeltaSet> = vec![];
-    let mut all_coords = vec![];
-
-    for (ix, master) in contours.iter().enumerate() {
-        if let Some(master) = master {
-            // If this is not a sparse master, we have a width and a set of coordinates.
-            let width = widths[ix].unwrap();
-            // Flatten all points (i.e. combine all contours together) in the glyph
-            // and split up X and Y into separate arrays.
-            let (mut master_x_coords, mut master_y_coords): (Vec<f32>, Vec<f32>) = master
-                .iter()
-                .flatten()
-                .map(|pt| (pt.x as f32, pt.y as f32))
-                .unzip();
-
-            // Add the phantom points
-            master_x_coords.extend(vec![0_f32, width as f32, 0.0, 0.0]);
-            master_y_coords.extend(vec![0.0, 0.0, 0.0, 0.0]);
-
-            // Concat the X-coordinates/Y-coordinates in preparation for being
-            // reshaped into a 2d ndarray.
-            let len = master_x_coords.len();
-            master_x_coords.extend(master_y_coords);
-
-            all_coords.push(Some(
-                ndarray::Array2::from_shape_vec((2, len), master_x_coords)
-                    .unwrap()
-                    .reversed_axes(),
-            ));
-        } else {
-            all_coords.push(None);
-        }
-    }
-
-    // The model takes Vec<T> T:Sub, and ndarray::Array2 implements Sub,
-    // so we can just send the whole vec of ndarrays to the model and get
-    // back our deltas.
-    let deltas_and_supports = model.get_deltas_and_supports(&all_coords);
-
-    for (delta, support) in deltas_and_supports.iter() {
-        if support.is_empty() {
-            continue;
-        }
-
-        // Turn the ndarray back into a vec of tuples
-        let deltas: Vec<(i16, i16)> = delta
-            .mapv(|x| x as i16)
-            .outer_iter()
-            .map(|x| (x[0], x[1]))
-            .collect();
-
-        // The variation model gives us the tents for each deltaset
-        let tuples = model
-            .axis_order
-            .iter()
-            .map(|ax| support.get(ax).unwrap_or(&(0.0, 0.0, 0.0)))
-            .copied();
-        let (start, peak, end) = tuples.into_iter().unzip_n_vec();
-
-        // And we're done
-        deltasets.push(DeltaSet {
-            peak,
-            start,
-            end,
-            deltas,
-        })
-    }
-    GlyphVariationData { deltasets }
 }
