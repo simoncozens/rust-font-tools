fa7d90c80fe2a68b2108b8374c4f7ba17f76e4d5
Simon Cozens <simon@simon-cozens.org>
2024-06-06 11:31:54 +0100

    Revert "Drop RBF, use OT interpolation"
    This reverts commit 0024c8a89c6daa2d572228b990528a4efbe52894.

--- triangulate/src/interpolate.rs
+++ triangulate/src/interpolate.rs
@@ -1,48 +1,46 @@
 use nalgebra::DVector;
 use otmath::{Location, VariationModel};
+use rbf_interp::Scatter;
 
 fn interpolate<T: Into<f64> + Copy>(
     numbers: &[Option<ndarray::Array1<T>>],
     model: &VariationModel<String>,
     location: &Location<String>,
-) -> ndarray::Array1<f32> {
+) -> ndarray::Array1<f64> {
     // log::debug!("Interpolating {:?} at {:?}", numbers, location);
 
     let locations = &model.original_locations;
-    let mut vals: Vec<DVector<f32>> = vec![];
-    let mut master_locations = vec![];
+    let mut vals: Vec<DVector<f64>> = vec![];
+    let axis_count = location.len();
+    let mut centers: Vec<DVector<f64>> = vec![];
     for (maybe_number, master_location) in numbers.iter().zip(locations.iter()) {
         if let Some(number) = maybe_number {
-            let this_val_vec: Vec<f32> =
-                number.to_vec().iter().map(|x| (*x).into() as f32).collect();
+            let this_location: DVector<f64> = DVector::from_fn(axis_count, |i, _| {
+                let axis = model
+                    .axis_order
+                    .get(i)
+                    .expect("Location had wrong axis count?");
+                let val = master_location.get(axis).expect("Axis not found?!");
+                *val as f64
+            });
+            centers.push(this_location);
+            let this_val_vec: Vec<f64> = number.to_vec().iter().map(|x| (*x).into()).collect();
             let this_val = DVector::from_vec(this_val_vec);
             vals.push(this_val);
-            master_locations.push(master_location.clone());
         }
     }
-    if master_locations.len() != locations.len() {
-        VariationModel::new(master_locations, model.axis_order.clone())
-            .interpolate_from_deltas(location, &vals)
-    } else {
-        model.interpolate_from_deltas(location, &vals)
-    }
-    .unwrap()
-    .as_slice()
-    .to_owned()
-    .into()
+    let scatter = Scatter::create(centers, vals, rbf_interp::Basis::PolyHarmonic(1), 2);
 
-    // let scatter = Scatter::create(centers, vals, rbf_interp::Basis::PolyHarmonic(1), 2);
-
-    // let coords = DVector::from_fn(axis_count, |i, _| {
-    //     let axis = model
-    //         .axis_order
-    //         .get(i)
-    //         .expect("Location had wrong axis count?");
-    //     let val = location.get(axis).expect("Axis not found?!");
-    //     *val as f64
-    // });
-    // let deltas: Vec<f64> = scatter.eval(coords).as_slice().to_owned();
-    // deltas.into()
+    let coords = DVector::from_fn(axis_count, |i, _| {
+        let axis = model
+            .axis_order
+            .get(i)
+            .expect("Location had wrong axis count?");
+        let val = location.get(axis).expect("Axis not found?!");
+        *val as f64
+    });
+    let deltas: Vec<f64> = scatter.eval(coords).as_slice().to_owned();
+    deltas.into()
 }
 
 pub(crate) fn interpolate_field<T: core::fmt::Debug>(
@@ -68,8 +66,8 @@ pub(crate) fn interpolate_field<T: core::fmt::Debug>(
             })
         })
         .collect();
-    let deltas: ndarray::Array1<f64> = interpolate(&deltas, model, location).map(|&x| f64::from(x));
-    let new_values: ndarray::Array1<f64> = default_numbers + deltas;
+    let deltas = interpolate(&deltas, model, location);
+    let new_values = default_numbers + deltas;
     // default_numbers + values
     setter(object, new_values.as_slice().expect("Couldn't get slice"))
 }
