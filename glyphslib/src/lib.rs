c4d656473db2a662529c0adbe6cec8cdcc96597a
Simon Cozens <simon@simon-cozens.org>
2024-12-19 22:56:36 +0000

    Begin to be a bit more useful and serious

--- glyphslib/src/lib.rs
+++ glyphslib/src/lib.rs
@@ -1,27 +1,29 @@
-mod glyphs3;
+pub mod glyphs3;
 use std::{fs, path};
 
 use glyphs3::Glyphs3;
 use openstep_plist::de::Deserializer;
 use openstep_plist::Plist;
-use serde::Deserialize;
 
-enum Font {
+pub enum Font {
     Glyphs3(Glyphs3),
 }
 impl Font {
-    fn load(glyphs_file: &path::Path) -> Self {
-        let raw_content = fs::read_to_string(glyphs_file).unwrap(); // I have no time to be tidy
-        let plist = Plist::parse(&raw_content).unwrap();
-        let mut deserializer = &mut Deserializer::from_plist(&plist);
-        let result: Result<Glyphs3, _> = serde_path_to_error::deserialize(deserializer);
-        let glyphs3 = result.unwrap();
-        Font::Glyphs3(glyphs3)
+    pub fn load(glyphs_file: &path::Path) -> Result<Self, Box<dyn std::error::Error>> {
+        let raw_content = fs::read_to_string(glyphs_file)?;
+        Self::load_str(&raw_content)
     }
-
-    fn as_glyphs3(&self) -> &Glyphs3 {
+    pub fn load_str(raw_content: &str) -> Result<Self, Box<dyn std::error::Error>> {
+        let plist = Plist::parse(raw_content).unwrap();
+        let deserializer = &mut Deserializer::from_plist(&plist);
+        let result: Result<Glyphs3, _> = serde_path_to_error::deserialize(deserializer);
+        let glyphs3 = result?;
+        Ok(Font::Glyphs3(glyphs3))
+    }
+    pub fn as_glyphs3(&self) -> Option<&Glyphs3> {
         match self {
-            Font::Glyphs3(glyphs3) => glyphs3,
+            Font::Glyphs3(glyphs3) => Some(glyphs3),
+            _ => None,
         }
     }
 }
